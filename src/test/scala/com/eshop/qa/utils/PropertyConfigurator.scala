4e116a96d7aa5916fcc1427fac5a3265e9b708a8
SviatoslavPaseka <paseka.syat@gmail.com>
2024-01-22 11:07:25 +0200

    refactoring1

--- src/test/scala/com/eshop/qa/utils/PropertyConfigurator.scala
+++
@@ -1,9 +0,0 @@
-package com.eshop.qa.utils
-
-object PropertyConfigurator {
-  def getProperty(propertyName: String, defaultValue: String): String = {
-    Option(System.getenv(propertyName))
-      .orElse(Option(System.getProperty(propertyName)))
-      .getOrElse(defaultValue)
-  }
-}
